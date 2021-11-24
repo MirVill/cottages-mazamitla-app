f50bf99b2d24097affab4f5101f99c0d5b48aac8
Miriam <miriamvillazaragoza@gmail.com>
2021-11-24 14:37:16 -0600

    Made Footer

---
+++ src/Layouts/Footer.js
@@ -0,0 +1,13 @@
+const Footer = () => {
+  return (
+    <footer className='footer' >
+      <ul>
+        <li>¿Quienes somos?</li>
+        <li>¿Donde nos encontramos?</li>
+        <li>Reservaciones</li>
+      </ul>
+    </footer>
+  );
+};
+
+export default Footer;
