9f14114e1c0c9e7ccee2f48f2248097f2375bd3e
Miriam <miriamvillazaragoza@gmail.com>
2021-11-24 14:37:49 -0600

    Build main layout

---
+++ src/Layouts/MainLayout.js
@@ -0,0 +1,17 @@
+import { Fragment } from "react";
+import Footer from "./Footer";
+import Header from "./Header";
+
+const MainLayout = (props) => {
+  return (
+    <Fragment>
+      <Header />
+      <main className='background' >
+        {props.children}
+      </main>
+      <Footer />
+    </Fragment>
+  );
+}; 
+
+export default MainLayout;
