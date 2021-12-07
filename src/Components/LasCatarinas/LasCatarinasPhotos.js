5f69cfe7814ad40cb407d440648b627b0b787467
Miriam <miriamvillazaragoza@gmail.com>
2021-12-07 00:07:36 -0600

    imported photos

---
+++ src/Components/LasCatarinas/LasCatarinasPhotos.js
@@ -0,0 +1,31 @@
+import Image1 from "../../Assets/Images/cottage-photos/Imagen1.jpg";
+import Image2 from "../../Assets/Images/cottage-photos/Imagen2.jpg";
+import Image3 from "../../Assets/Images/cottage-photos/Imagen3.jpg";
+import Image4 from "../../Assets/Images/cottage-photos/Imagen4.jpg";
+import Image5 from "../../Assets/Images/cottage-photos/Imagen5.jpg";
+import Image6 from "../../Assets/Images/cottage-photos/Imagen6.jpg";
+import Image7 from "../../Assets/Images/cottage-photos/Imagen7.jpg";
+import Image8 from "../../Assets/Images/cottage-photos/Imagen8.jpg";
+import Image9 from "../../Assets/Images/cottage-photos/Imagen9.jpg";
+import Image10 from "../../Assets/Images/cottage-photos/Imagen10.jpg";
+import Image11 from "../../Assets/Images/cottage-photos/Imagen11.jpg";
+import Image12 from "../../Assets/Images/cottage-photos/Imagen12.jpg";
+
+export default [
+  Image3,
+  Image1,
+  Image2,
+  Image3,
+  Image1,
+  Image2,
+  Image3,
+  Image1,
+  Image2,
+  Image3,
+  Image1,
+  Image2,
+  Image3,
+  Image1,
+  Image2,
+  Image3,
+];
