715ae7e54e1e8a409336e59538ad629ba3a1cef5
Miriam <miriamvillazaragoza@gmail.com>
2021-12-07 19:42:51 -0600

    Added resevations btn

--- src/Containers/LasCatarinasData.js
+++ src/Containers/LasCatarinasData.js
@@ -1,9 +1,11 @@
 import Gallery from '../Components/Gallery';
 import InformationLasCatarinas from '../Components/LasCatarinas/InformationLasCatarinas';
+import Button from '../Utils/Button';
 const LasCatarinasData = () => {
   return <div>
     <Gallery /> 
     <InformationLasCatarinas />
+    <Button text='Reservar'/>
   </div>
 };
 
---
+++ src/Utils/Button.js
@@ -0,0 +1,7 @@
+const Button = (props) => {
+  return <button className='btn' >
+    {props.text}
+  </button>
+};
+
+export default Button;
