9a37036454c3ba7dd33d77c08263ff5f444c9f5e
Miriam <miriamvillazaragoza@gmail.com>
2021-12-09 21:45:38 -0600

    Added navlinks

--- src/Containers/LasCatarinasData.js
+++ src/Containers/LasCatarinasData.js
@@ -1,12 +1,13 @@
 import InformationLasCatarinas from '../Components/LasCatarinas/InformationLasCatarinas';
 import Button from '../Utils/Button';
 import LasCatarinasGallery from '../Components/LasCatarinas/LasCatarinasGallery';
+import { NavLink } from 'react-router-dom';
 const LasCatarinasData = () => {
   return <div className='cottage-page-container' >
     <h1 className='page-title' >Las Catarinas</h1>
     <LasCatarinasGallery /> 
     <InformationLasCatarinas />
-    <Button text='Reservar'/>
+    <NavLink to='/contactos' ><Button text='Reservar'/></NavLink>
   </div>
 };
 
