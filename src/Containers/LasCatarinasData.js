00e2fdac1d52132261c8e3b08cdcec0ddef2f074
Miriam <miriamvillazaragoza@gmail.com>
2021-12-09 20:10:03 -0600

    merged btn

--- src/Containers/LasCatarinasData.js
+++ src/Containers/LasCatarinasData.js
@@ -1,10 +1,12 @@
 import InformationLasCatarinas from '../Components/LasCatarinas/InformationLasCatarinas';
+import Button from '../Utils/Button';
 import LasCatarinasGallery from '../Components/LasCatarinas/LasCatarinasGallery';
 const LasCatarinasData = () => {
   return <div className='cottage-page-container' >
     <h1 className='page-title' >Las Catarinas</h1>
     <LasCatarinasGallery /> 
     <InformationLasCatarinas />
+    <Button text='Reservar'/>
   </div>
 };
 
